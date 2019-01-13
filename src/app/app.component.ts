ad45aaa2744fbf528d35ce30c0e3d879ba910eef
Nomdeplum Vovan <tuzenkov50@gmail.com>
2019-01-13 12:55:49 +0200

    debounce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { Observable, fromEvent, interval, timer, of } from 'rxjs';
-import { merge, flatMap, takeUntil, scan, map, switchMap, tap, reduce, takeLast, startWith, skipUntil } from 'rxjs/operators';
-
+import { merge, takeUntil, scan, map, switchMap, tap } from 'rxjs/operators';
+import {  takeLast, auditTime, debounceTime, skip } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -22,7 +22,12 @@ export class AppComponent implements OnInit, OnDestroy {
     const startButton$ = fromEvent(document.getElementById('start'), 'click');
     const stopButton$ = fromEvent(document.getElementById('stop'), 'click');
     const resetButton$ = fromEvent(document.getElementById('reset'), 'click');
-    const waitButton$ = fromEvent(document.getElementById('wait'), 'click');
+    const waitButton$ = fromEvent(document.getElementById('wait'), 'click')
+      .pipe( skip(1),
+       tap(val => console.log('wait', val)),
+        // takeUntil(timer(300))
+        );
+
 
     this.timer$ = of()
       .pipe(
@@ -42,7 +47,6 @@ export class AppComponent implements OnInit, OnDestroy {
             map(() => 0)
           ),
           waitButton$.pipe(
-            skipUntil(timer(0, 300)),
             takeLast(1),
             map(() => 0))
         ),
@@ -69,38 +73,8 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnInit() {
     // this.startTimer();
   }
-  // }
+
   ngOnDestroy() {
 
   }
 }
-// this.timer$ = of()
-//       .pipe(
-//         merge(
-//           interval(1000).pipe(
-//             takeUntil(stopButton$),
-//             takeUntil(resetButton$),
-//             takeUntil(waitButton$),
-//             map(() => 1),
-//           ),
-//           resetButton$.pipe(map(() => 0)),
-//           waitButton$.pipe(
-//             takeLast(1),
-//             map(() => 0)
-//           ),
-//         ),
-//         scan((acc: number, val: number) => val === 0 ? 0 : acc + val),
-//         tap(val => console.log('scan', val)),
-//       );
-
-//     this.timer$.subscribe((value: number) => {
-//       this.timerValue = value;
-//       this.timerValueSecond = (this.timerValue % 60) < 10 ? '0' + (this.timerValue % 60) : (this.timerValue % 60);
-
-//       this.timerValueMinute = Math.floor((this.timerValue / 60) % 60);
-//       this.timerValueMinute = this.timerValueMinute < 10 ? '0' + this.timerValueMinute : this.timerValueMinute;
-
-
-//       this.timerValueHour = Math.floor(this.timerValue / 3600);
-//       this.timerValueHour = this.timerValueHour < 10 ? '0' + this.timerValueHour : this.timerValueHour;
-//     });
